a0c045bd6551143e2a86efcf9cfdba4f68b2320e
Vova <berezkin@mail.ru>
2019-09-10 13:00:28 +0300

    Add loading progress

--- app/src/main/java/berezkin/giphy/ui/main/MainAdapter.kt
+++ app/src/main/java/berezkin/giphy/ui/main/MainAdapter.kt
@@ -1,5 +1,6 @@
 package berezkin.giphy.ui.main
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +10,42 @@ import androidx.recyclerview.widget.RecyclerView
 import berezkin.giphy.R
 import berezkin.giphy.repo.MainItem
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.main_item.view.*
 
 class MainViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
   fun bind(item: MainItem?) {
+    view.progress.hide()
     view.title.text = item?.title
     val thumbnail = Glide.with(view).load(item?.stillUrl)
     thumbnail.into(view.image)
-    view.image.setOnClickListener { //  TODO Loading progress
-      Glide.with(view).load(item?.url).thumbnail(thumbnail).into(view.image)
+    view.image.setOnClickListener {
+      view.progress.show()
+      Glide.with(view).load(item?.url).listener(object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+          e: GlideException?,
+          model: Any?,
+          target: Target<Drawable>?,
+          isFirstResource: Boolean
+        ): Boolean {
+          view.progress.hide()
+          return false
+        }
+
+        override fun onResourceReady(
+          resource: Drawable?,
+          model: Any?,
+          target: Target<Drawable>?,
+          dataSource: DataSource?,
+          isFirstResource: Boolean
+        ): Boolean {
+          view.progress.hide()
+          return false
+        }
+      }).thumbnail(thumbnail).into(view.image)
     }
   }
 }
